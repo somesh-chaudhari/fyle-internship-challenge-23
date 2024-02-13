15bda0eec17e21b46f8a7b92d8547552c4769e27
Somesh Chaudhari <113702243+somesh-chaudhari@users.noreply.github.com>
2024-02-13 07:43:11 +0000

    build-done

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -3,30 +3,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const GITHUB_ACCESS_TOKEN = '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';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
   private apiUrl = 'https://api.github.com';
-  private headers = new HttpHeaders().set('Authorization', `token ${GITHUB_ACCESS_TOKEN}`);
+  // private headers = new HttpHeaders().set('Authorization', `token ${GITHUB_ACCESS_TOKEN}`);
 
   constructor(private http: HttpClient) { }
 
   getUser(username: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/users/${username}`, { headers: this.headers });
+    return this.http.get(`${this.apiUrl}/users/${username}`);
   }
 
   getUserRepos(username: string, page: number = 1, perPage: number = 10): Observable<any[]> {
     const params = new HttpParams().set('page', page.toString()).set('per_page', perPage.toString());
-    return this.http.get<any[]>(`${this.apiUrl}/users/${username}/repos`, { headers: this.headers, params });
+    return this.http.get<any[]>(`${this.apiUrl}/users/${username}/repos`, { params });
   }
 
   getRepoDetails(owner: string, repoName: string, page: number = 1, perPage: number = 10): Observable<any> {
-    return this.http.get(`${this.apiUrl}/repos/${owner}/${repoName}`, { headers: this.headers });
+    return this.http.get(`${this.apiUrl}/repos/${owner}/${repoName}`);
   }
 
   getRepoLanguages(owner: string, repoName: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/repos/${owner}/${repoName}/languages`, { headers: this.headers });
+    return this.http.get(`${this.apiUrl}/repos/${owner}/${repoName}/languages`);
   }
 }
