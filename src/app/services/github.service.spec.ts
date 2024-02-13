d0b31aa62ab21f663fb79b79e30bdf1462c118cc
Somesh Chaudhari <113702243+somesh-chaudhari@users.noreply.github.com>
2024-02-13 16:18:38 +0000

    test-cases-added

---
+++ src/app/services/github.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed } from '@angular/core/testing';
+import { GithubService } from './github.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe('GithubService', () => {
+  let service: GithubService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GithubService]
+    });
+    service = TestBed.inject(GithubService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch user details', () => {
+    const dummyUser = { name: 'John Doe', login: 'johndoe' };
+
+    service.getUser('johndoe').subscribe(user => {
+      expect(user).toEqual(dummyUser);
+    });
+
+    const req = httpTestingController.expectOne('https://api.github.com/users/johndoe');
+    expect(req.request.method).toEqual('GET');
+    req.flush(dummyUser);
+  });
+
+  // Add more test cases as needed
+});
--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,21 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
+import { GithubService } from '../services/github.service';
+import { of } from 'rxjs';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let githubServiceSpy: jasmine.SpyObj<GithubService>;
+
+  beforeEach(async () => {
+    const githubServiceSpyObj = jasmine.createSpyObj('GithubService', ['getUser', 'getUserRepos']);
+
+    await TestBed.configureTestingModule({
+      declarations: [UserComponent],
+      providers: [
+        { provide: GithubService, useValue: githubServiceSpyObj }
+      ]
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserComponent]
-    });
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    githubServiceSpy = TestBed.inject(GithubService) as jasmine.SpyObj<GithubService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch user details', () => {
+    const user = { name: 'John Doe', login: 'johndoe' };
+    githubServiceSpy.getUser.and.returnValue(of(user));
+
+    component.username = 'johndoe';
+    component.getUserDetails();
+
+    expect(component.user).toEqual(user);
+  });
+
+  // Add more test cases as needed
 });
